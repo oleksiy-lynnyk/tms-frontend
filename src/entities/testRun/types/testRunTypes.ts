96b681256d4ebf9ccd8d63961d286c0fafdb7a2e
unknown <olynnyk269@gmail.com>
2025-11-22 19:07:57 +0200

    Add milestoneId to TestRunDTO

--- src/entities/testRun/types/testRunTypes.ts
+++ src/entities/testRun/types/testRunTypes.ts
@@ -14,6 +14,7 @@ export interface TestRunDTO {
     environmentId?: string;
     configurationId?: string;
     versionId?: string;
+    milestoneId?: string;
     environmentName?: string;
     configurationName?: string;
     versionName?: string;
